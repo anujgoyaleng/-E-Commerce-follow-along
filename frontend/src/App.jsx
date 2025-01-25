091baf284a704a7a83974b5a64e39aa722e9df38
anujgoyaleng <anuj.goyal@kalvium.community>
2025-01-25 10:20:54 +0530

    added signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"; 
+import {LoginPage,SignupPage} from './Routes.jsx';
 
 import './App.css';
-import LoginPage from "./pages/Login";
 
 function App(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/Login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
 
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from "../component/auth/Signup"
+function SignupPage() {
+    return (
+      <div>
+        <Signup />
+
+      </div>
+    )
+}
+export default SignupPage;
